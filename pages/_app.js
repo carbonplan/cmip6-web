ae3f7ddfda71167bd8191de5bb94a5ee2c7fcaa3
Kata Martin <katasm@gmail.com>
2021-12-01 13:06:02 -0800

    Remove special breakpoints

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { ThemeProvider } from 'theme-ui'
 import '@carbonplan/components/fonts.css'
 import '@carbonplan/components/globals.css'
-import theme from '../theme'
+import theme from '@carbonplan/theme'
 import { RegionProvider } from '../components/region'
 
 const App = ({ Component, pageProps }) => {
