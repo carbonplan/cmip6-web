8dc65109df61bf80222433953d3ab6ca1b226d73
Kata Martin <katasm@gmail.com>
2021-12-02 10:01:12 -0800

    Rough implementation using only event

--- components/datasets/display-section.js
+++ components/datasets/display-section.js
@@ -1,11 +1,18 @@
 import { Box } from 'theme-ui'
+import { useEffect, useRef, useState } from 'react'
 import { Group } from '@carbonplan/components'
+import { DraggableCore } from 'react-draggable'
 
 import Section from '../section'
 import { useDataset, useSelectedDatasets } from './store'
 import ControlPanelDivider from '../control-panel-divider'
 
 const DatasetDisplay = ({ name, sx }) => {
+  const [rel, setRel] = useState(null)
+  const [initialTop, setInitialTop] = useState(null)
+  const [top, setTop] = useState(0)
+
+  const container = useRef(null)
   const { dataset, reorderDataset } = useDataset(name)
   const {
     display: { colormapName, clim, opacity },
@@ -21,19 +28,53 @@ const DatasetDisplay = ({ name, sx }) => {
     }
   }
 
+  const dragging = typeof rel === 'number'
+
   return (
-    <Box onKeyDown={handleKeyDown} tabIndex={0}>
-      <Group spacing={4}>
-        <ControlPanelDivider />
-        <Section sx={sx.heading} label={shortName}>
-          <Group spacing={2}>
-            <Box>Colormap: {colormapName}</Box>
-            <Box>Color range: {clim.join(', ')}</Box>
-            <Box>Opacity: {opacity}</Box>
+    <>
+      <DraggableCore
+        onStart={(d) => {
+          //   console.log(e, d)
+          //   e.stopPropagation()
+          setInitialTop(container.current.offsetTop)
+          setRel(e.pageY - container.current.offsetTop)
+        }}
+        onDrag={(d) => {
+          setTop(d.pageY - rel)
+        }}
+        onStop={(d) => {
+          const shiftY = top - initialTop
+          const delta = shiftY / container.current.offsetHeight
+
+          reorderDataset(Math.round(delta))
+          setRel(null)
+          setInitialTop(null)
+          setTop(0)
+        }}
+      >
+        <Box
+          onKeyDown={handleKeyDown}
+          tabIndex={0}
+          ref={container}
+          sx={{
+            top: top + 'px',
+            position: dragging ? 'absolute' : 'relative',
+          }}
+        >
+          <Group spacing={4}>
+            <ControlPanelDivider />
+            <Section sx={sx.heading} label={shortName}>
+              <Group spacing={2}>
+                <Box>Colormap: {colormapName}</Box>
+                <Box>Color range: {clim.join(', ')}</Box>
+                <Box>Opacity: {opacity}</Box>
+              </Group>
+            </Section>
           </Group>
-        </Section>
-      </Group>
-    </Box>
+        </Box>
+      </DraggableCore>
+      {dragging && <Box sx={{ height: container.current.offsetHeight }} />}
+    </>
   )
 }
 
