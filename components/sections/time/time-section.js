6dc201f413d11884714495c322ff41799f409ef2
Kata Martin <katasm@gmail.com>
2022-02-15 15:48:13 -0800

    Use new SidePanel layout components

--- components/sections/time/time-section.js
+++ components/sections/time/time-section.js
@@ -1,5 +1,6 @@
 import { Box } from 'theme-ui'
-import { alpha } from '@theme-ui/color'
+import { SidePanelFooter } from '@carbonplan/layouts'
+
 import { useDatasetsStore } from '../../datasets'
 import Sliders from './sliders'
 import SlidersDisabled from './sliders-disabled'
@@ -30,20 +31,14 @@ const TimeSection = ({ sx }) => {
   }
 
   return (
-    <Box
+    <SidePanelFooter
       sx={{
-        px: [4, 5, 5, 6],
-        transition: 'background-color 0.15s',
-        cursor: disabled ? 'default' : 'pointer',
-        '@media (hover: hover) and (pointer: fine)': {
-          '&:hover': { bg: disabled ? 'transparent' : alpha('muted', 0.25) },
-        },
+        pointerEvents: !disabled ? 'all' : 'none',
         pt: [3],
-        pb: [4],
       }}
     >
       {inner}
-    </Box>
+    </SidePanelFooter>
   )
 }
 
