24510c45091eb63527b1e57f637845ed02ac4dae
Kata Martin <katasm@gmail.com>
2022-03-31 16:12:59 -0700

    Fix tbody

--- components/sections/query/dataset-info.js
+++ components/sections/query/dataset-info.js
@@ -56,69 +56,70 @@ const DatasetInfo = ({ dataset, color }) => {
         sx={{ bg: alpha(color, 0.25), my: 2, py: 1, px: '24px' }}
       >
         <Box as='table' sx={{ display: 'block' }}>
-          <Box as='tbody' sx={{ display: 'block' }}></Box>
-          <Row as='tr' columns={[6, 8, 4, 4]} sx={sx.row}>
-            <Column as='td' start={[1]} width={[3, 2, 2, 2]} sx={sx.index}>
-              Institution
-            </Column>
-            <Column
-              as='td'
-              start={[4, 3, 3, 3]}
-              width={[3, 2, 2, 3]}
-              sx={sx.entry}
-            >
-              {dataset.institution}
-            </Column>
-          </Row>
-          <Row as='tr' columns={[6, 8, 4, 4]} sx={sx.row}>
-            <Column as='td' start={[1]} width={[3, 2, 2, 2]} sx={sx.index}>
-              Member
-            </Column>
-            <Column
-              as='td'
-              start={[4, 3, 3, 3]}
-              width={[3, 2, 2, 3]}
-              sx={sx.entry}
-            >
-              {dataset.member}
-            </Column>
-          </Row>
-          <Row as='tr' columns={[6, 8, 4, 4]} sx={sx.row}>
-            <Column as='td' start={[1]} width={[3, 2, 2, 2]} sx={sx.index}>
-              Aggregation
-            </Column>
-            <Column
-              as='td'
-              start={[4, 3, 3, 3]}
-              width={[3, 2, 2, 3]}
-              sx={sx.entry}
-            >
-              {dataset.aggregation}
-            </Column>
-          </Row>
-          <Row
-            as='tr'
-            columns={[6, 8, 4, 4]}
-            sx={{ ...sx.row, borderBottomWidth: 0 }}
-          >
-            <Column as='td' start={1} width={[6, 8, 4, 4]} sx={sx.entry}>
-              <Button
-                prefix={<Down />}
-                sx={{
-                  color: alpha(color, 0.75),
-                  '@media (hover: hover) and (pointer: fine)': {
-                    '&:hover': {
-                      color,
-                    },
-                  },
-                }}
-                onClick={handleClick}
-                size='xs'
+          <Box as='tbody' sx={{ display: 'block' }}>
+            <Row as='tr' columns={[6, 8, 4, 4]} sx={sx.row}>
+              <Column as='td' start={[1]} width={[3, 2, 2, 2]} sx={sx.index}>
+                Institution
+              </Column>
+              <Column
+                as='td'
+                start={[4, 3, 3, 3]}
+                width={[3, 2, 2, 3]}
+                sx={sx.entry}
               >
-                {copied ? 'Copied!' : 'Zarr store'}
-              </Button>
-            </Column>
-          </Row>
+                {dataset.institution}
+              </Column>
+            </Row>
+            <Row as='tr' columns={[6, 8, 4, 4]} sx={sx.row}>
+              <Column as='td' start={[1]} width={[3, 2, 2, 2]} sx={sx.index}>
+                Member
+              </Column>
+              <Column
+                as='td'
+                start={[4, 3, 3, 3]}
+                width={[3, 2, 2, 3]}
+                sx={sx.entry}
+              >
+                {dataset.member}
+              </Column>
+            </Row>
+            <Row as='tr' columns={[6, 8, 4, 4]} sx={sx.row}>
+              <Column as='td' start={[1]} width={[3, 2, 2, 2]} sx={sx.index}>
+                Aggregation
+              </Column>
+              <Column
+                as='td'
+                start={[4, 3, 3, 3]}
+                width={[3, 2, 2, 3]}
+                sx={sx.entry}
+              >
+                {dataset.aggregation}
+              </Column>
+            </Row>
+            <Row
+              as='tr'
+              columns={[6, 8, 4, 4]}
+              sx={{ ...sx.row, borderBottomWidth: 0 }}
+            >
+              <Column as='td' start={1} width={[6, 8, 4, 4]} sx={sx.entry}>
+                <Button
+                  prefix={<Down />}
+                  sx={{
+                    color: alpha(color, 0.75),
+                    '@media (hover: hover) and (pointer: fine)': {
+                      '&:hover': {
+                        color,
+                      },
+                    },
+                  }}
+                  onClick={handleClick}
+                  size='xs'
+                >
+                  {copied ? 'Copied!' : 'Zarr store'}
+                </Button>
+              </Column>
+            </Row>
+          </Box>
         </Box>
       </Column>
     </Row>
