b352e909cad790f845e20dfb1cb8fdfa75d8bd65
Kata Martin <katasm@gmail.com>
2022-05-31 15:41:26 -0700

    Additionally track filters

--- components/use-routing.js
+++ components/use-routing.js
@@ -7,18 +7,52 @@ import { useDatasetsStore } from './datasets'
 const useRouting = () => {
   const router = useRouter()
   const initialized = useDatasetsStore((state) => !!state.datasets)
-  const { name: active, dateStrings } =
-    useDatasetsStore(
-      (state) => state.datasets && state.datasets[state.active]
-    ) || {}
+  const active = useDatasetsStore((state) => state.active)
   const displayTime = useDatasetsStore((state) => state.displayTime)
+  const variable = useDatasetsStore((state) => state.filters?.variable)
+  const timescale = useDatasetsStore((state) => state.filters?.timescale)
+  const experiment = useDatasetsStore((state) => state.filters?.experiment)
   const setActive = useDatasetsStore((state) => state.setActive)
   const setDisplayTime = useDatasetsStore((state) => state.setDisplayTime)
+  const setFilters = useDatasetsStore((state) => state.setFilters)
 
   // Sets values in the store based on the initial URL parameters
   useEffect(() => {
     if (router.isReady && initialized) {
-      const { active, year, month, day } = router.query
+      const {
+        active,
+        variable,
+        timescale,
+        // displayTime values
+        year,
+        month,
+        day,
+        // experiment values
+        historical,
+        ssp245,
+        ssp370,
+        ssp585,
+      } = router.query
+
+      const filters = {
+        ...(variable ? { variable } : {}),
+        ...(timescale ? { timescale } : {}),
+        ...(historical || ssp245 || ssp370 || ssp585
+          ? {
+              experiment: {
+                historical: historical === 'true',
+                ssp245: ssp245 === 'true',
+                ssp370: ssp370 === 'true',
+                ssp585: ssp585 === 'true',
+              },
+            }
+          : {}),
+      }
+
+      if (Object.keys(filters).length > 0) {
+        setFilters(filters)
+      }
+
       if (active) {
         setActive(active)
       }
@@ -35,13 +69,16 @@ const useRouting = () => {
       const query = {
         ...(active ? { active } : {}),
         ...(displayTime ? displayTime : {}),
+        ...(experiment ? experiment : {}),
+        ...(variable ? { variable } : {}),
+        ...(timescale ? { timescale } : {}),
       }
 
       router.replace({ pathname: '', query }, null, {
         shallow: true,
       })
     }
-  }, [initialized, active, displayTime])
+  }, [initialized, active, displayTime, variable, timescale, experiment])
 }
 
 export default useRouting
