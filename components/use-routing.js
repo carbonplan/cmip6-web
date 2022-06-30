f16e44a424282b6b285e285862abc429f53993ca
freeman-lab <the.freeman.lab@gmail.com>
2022-06-30 11:41:18 -0700

    fix for new catalog

--- components/use-routing.js
+++ components/use-routing.js
@@ -4,7 +4,7 @@ import { useDatasetsStore } from './datasets'
 import { encodeJSON, decodeHex } from './utils'
 
 const DEFAULT_ACTIVE =
-  'ScenarioMIP.CCCma.CanESM5.ssp245.r1i1p1f1.year.DeepSD-BC.tasmax'
+  'ScenarioMIP.CCCma.CanESM5.ssp245.r1i1p1f1.day.DeepSD-BC.tasmax.year'
 
 const validateQuery = (query, datasets) => {
   const {
