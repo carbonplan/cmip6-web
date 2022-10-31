afc5644952300fba8e904d4b1eaa4436241487eb
katamartin <katasm@gmail.com>
2022-10-31 15:21:51 -0700

    Upgrade dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,17 @@ import { ThemeProvider } from 'theme-ui'
 import '@carbonplan/components/fonts.css'
 import '@carbonplan/components/globals.css'
 import '@carbonplan/maps/mapbox.css'
+import { MDXProvider, useMDXComponents } from '@mdx-js/react'
+import { useThemedStylesWithMdx } from '@theme-ui/mdx'
 import theme from '../theme'
 
 const App = ({ Component, pageProps }) => {
+  const components = useThemedStylesWithMdx(useMDXComponents())
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+      <MDXProvider components={components}>
+        <Component {...pageProps} />
+      </MDXProvider>
     </ThemeProvider>
   )
 }
